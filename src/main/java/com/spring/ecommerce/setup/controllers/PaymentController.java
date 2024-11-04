635df56fcef198734ac41ddbcb46a3916980825c
Luca <lucadebar33@gmail.com>
2024-11-04 16:42:03 +0100

    add : stripe successfully added api key and code to create a session.

---
+++ src/main/java/com/spring/ecommerce/setup/controllers/PaymentController.java
@@ -0,0 +1,27 @@
+package com.spring.ecommerce.setup.controllers;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    @PostMapping("/create-checkout-session")
+    public Session createCheckoutSession() throws StripeException {
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setSuccessUrl("https://example.com/success")
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setPrice("price_1MotwRLkdIwHu7ixYcPLm5uZ")
+                                        .setQuantity(2L)
+                                        .build()
+                        )
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .build();
+        return Session.create(params);
+    }
+}
