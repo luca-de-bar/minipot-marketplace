3ce3479cf81a668222beb165ed75fa2a71137c75
Luca <lucadebar33@gmail.com>
2024-11-04 17:23:42 +0100

    add : minor changes

--- src/main/java/com/spring/ecommerce/setup/controllers/PaymentController.java
+++ src/main/java/com/spring/ecommerce/setup/controllers/PaymentController.java
@@ -3,10 +3,14 @@ package com.spring.ecommerce.setup.controllers;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api/v1")
 public class PaymentController {
 
     @PostMapping("/create-checkout-session")
---
+++ src/main/java/com/spring/ecommerce/setup/controllers/ProductController.java
@@ -0,0 +1,5 @@
+package com.spring.ecommerce.setup.controllers;
+
+
+public class ProductController {
+}
