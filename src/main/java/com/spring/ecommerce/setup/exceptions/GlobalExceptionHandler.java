7ec64c538ede5ea55b1febe7060800dc2ecc03b4
luca-de-bar <lucadebar33@gmail.com>
2024-12-05 20:26:01 +0100

    add : exceptionHandler for login

--- src/main/java/com/spring/ecommerce/setup/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/spring/ecommerce/setup/exceptions/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
